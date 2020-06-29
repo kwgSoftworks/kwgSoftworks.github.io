982699f3ce348e7fec3262e0f4024828b39957e7
Ayoub Khadar <ayoub.khadar@tufts.edu>
2020-06-29 18:48:35 -0400

    Your awesome message

---
+++ build/precache-manifest.943d1187d06d140a49343385568c97e1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6683e6228ac6b5b7c2587afcec391b5b",
+    "url": "/kwgsoftworks/index.html"
+  },
+  {
+    "revision": "350bad1dfb0f1a3de901",
+    "url": "/kwgsoftworks/static/css/main.60982dcd.chunk.css"
+  },
+  {
+    "revision": "59b9c1c23fa774a0d8fd",
+    "url": "/kwgsoftworks/static/js/2.94a4c980.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kwgsoftworks/static/js/2.94a4c980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350bad1dfb0f1a3de901",
+    "url": "/kwgsoftworks/static/js/main.bf5bc32a.chunk.js"
+  },
+  {
+    "revision": "2699fb3cc93b8b82b475",
+    "url": "/kwgsoftworks/static/js/runtime-main.4ac229d9.js"
+  },
+  {
+    "revision": "f98b29e058d0de3b207b7a0786b89cb2",
+    "url": "/kwgsoftworks/static/media/App-Development.f98b29e0.svg"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "/kwgsoftworks/static/media/Black.de55ae52.otf"
+  },
+  {
+    "revision": "54cd463e645984b4d04bf6f7b0a6e890",
+    "url": "/kwgsoftworks/static/media/Bold.54cd463e.eot"
+  },
+  {
+    "revision": "c1d25eb210bffb2a14da7bde6bf9e630",
+    "url": "/kwgsoftworks/static/media/Bold.c1d25eb2.ttf"
+  },
+  {
+    "revision": "f9c9beff80bc81b7d15db3bfcc91f733",
+    "url": "/kwgsoftworks/static/media/Bold.f9c9beff.woff"
+  },
+  {
+    "revision": "0604a4b41f72c48c78fb36ae00530234",
+    "url": "/kwgsoftworks/static/media/Book.0604a4b4.woff"
+  },
+  {
+    "revision": "35955f9befef412927e2aa3b6bb5fa7a",
+    "url": "/kwgsoftworks/static/media/Book.35955f9b.ttf"
+  },
+  {
+    "revision": "61e157860b1429651ba26289da16cdaa",
+    "url": "/kwgsoftworks/static/media/Book.61e15786.eot"
+  },
+  {
+    "revision": "890785d7b7e19445bb485f48654f2271",
+    "url": "/kwgsoftworks/static/media/Business-Plans.890785d7.svg"
+  },
+  {
+    "revision": "8f15f7ac6897bba1ef2046d7a2cd13d0",
+    "url": "/kwgsoftworks/static/media/Developer-Documentation.8f15f7ac.svg"
+  },
+  {
+    "revision": "7908141b15113ce2512e6050ab89aa81",
+    "url": "/kwgsoftworks/static/media/Email.7908141b.svg"
+  },
+  {
+    "revision": "4216d45559a5b10f6eb2a21760f7083f",
+    "url": "/kwgsoftworks/static/media/Email_Logo.4216d455.svg"
+  },
+  {
+    "revision": "0fc871df6e79c271b01b73f237955f30",
+    "url": "/kwgsoftworks/static/media/Instagram.0fc871df.svg"
+  },
+  {
+    "revision": "3e8a9e8073b717a3908807627757ec1a",
+    "url": "/kwgsoftworks/static/media/Instagram_Logo.3e8a9e80.svg"
+  },
+  {
+    "revision": "f2d303102616940a26143e49dbaa176e",
+    "url": "/kwgsoftworks/static/media/LinkedIn.f2d30310.svg"
+  },
+  {
+    "revision": "c5eaa2b5c57c1933fdc769df957a8e37",
+    "url": "/kwgsoftworks/static/media/LinkedIn_Logo.c5eaa2b5.svg"
+  },
+  {
+    "revision": "022af2f62a7c0af68de51fa2cbf218e2",
+    "url": "/kwgsoftworks/static/media/Medium.022af2f6.ttf"
+  },
+  {
+    "revision": "7dbf3288fe8714f025ba6e564e7696e7",
+    "url": "/kwgsoftworks/static/media/Pitch_Decks.7dbf3288.svg"
+  },
+  {
+    "revision": "e9165c4d5560e036982d322d1d93d623",
+    "url": "/kwgsoftworks/static/media/Product-Marketing-Videos.e9165c4d.svg"
+  },
+  {
+    "revision": "70f7c1cfa6657a46ac9908283c737539",
+    "url": "/kwgsoftworks/static/media/Twitter.70f7c1cf.svg"
+  },
+  {
+    "revision": "0b55a8ab98f33f83e74f7fede448ee05",
+    "url": "/kwgsoftworks/static/media/Twitter_Logo.0b55a8ab.svg"
+  },
+  {
+    "revision": "60bd57c22848247e96de04eeae8816c7",
+    "url": "/kwgsoftworks/static/media/Web-Development.60bd57c2.svg"
+  },
+  {
+    "revision": "b0d1f09008360e2d170b4a38553287f4",
+    "url": "/kwgsoftworks/static/media/contact-header.b0d1f090.jpg"
+  },
+  {
+    "revision": "9483dec96846d9aa17ddbe377e54a4d4",
+    "url": "/kwgsoftworks/static/media/header1.9483dec9.jpg"
+  },
+  {
+    "revision": "e6cd7325219df20bde63a74215cba133",
+    "url": "/kwgsoftworks/static/media/header2.e6cd7325.jpg"
+  },
+  {
+    "revision": "01f2c9e1bbd1a4d8958dd32f75830700",
+    "url": "/kwgsoftworks/static/media/idea.01f2c9e1.gif"
+  }
+]);
